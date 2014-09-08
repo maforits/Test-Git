c679bce3918a2d3cc1ead78f0dcabd557c5a00f6
maforits <mauricio04@gmail.com>
2014-09-08 17:25:49 -0300

    added a test class

---
+++ Git Test/MVTest.m
@@ -0,0 +1,13 @@
+//
+//  MVTest.m
+//  Git Test
+//
+//  Created by Mauro on 9/8/14.
+//
+//
+
+#import "MVTest.h"
+
+@implementation MVTest
+
+@end
---
+++ MVTest.h
@@ -0,0 +1,13 @@
+//
+//  MVTest.h
+//  Git Test
+//
+//  Created by Mauro on 9/8/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MVTest : NSObject
+
+@end
